3cd237b551fa64bc8ef616c235b52eb39a1e1d9d
LucasBrentano <brentano.l@gmail.com>
2022-10-21 14:23:07 -0300

    basic player scene structure and some code progress

--- DinosaurRun/scenes/player/Player.gd
+++ DinosaurRun/scenes/player/Player.gd
@@ -1,2 +1,21 @@
 extends KinematicBody2D
+class_name Player
 
+signal died
+
+const JUMP_VELOCITY := -175
+const MAX_JUMP_VELOCITY := -1100
+
+var _gravity : float = 9.8
+var _can_jump := false
+var dead := false
+
+
+# Movement
+func _physics_process(delta: float) -> void:
+	if dead:
+		return
+
+
+	# if not is_on_floor():
+	# 	velocity.y += _gravity * delta
