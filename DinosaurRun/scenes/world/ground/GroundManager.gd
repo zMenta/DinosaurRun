8b909663cc207fe58e0ebf85f72284e8742bcdfa
LucasBrentano <brentano.l@gmail.com>
2022-11-04 11:49:56 -0300

    ground manager change speed function

--- DinosaurRun/scenes/world/ground/GroundManager.gd
+++ DinosaurRun/scenes/world/ground/GroundManager.gd
@@ -1,7 +1,12 @@
 extends Node2D
 
 onready var respawn_position := $Position2D
+onready var ground_group := $Grounds
 
 
 func _on_Ground_screen_exited(ground: Ground) -> void:
 	ground.global_position = respawn_position.global_position
+
+
+func change_speed(new_value: float) -> void:
+	ground_group.speed = new_value
