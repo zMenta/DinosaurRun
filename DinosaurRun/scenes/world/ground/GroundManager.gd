af29cc524dbf88aaa0be99a973e3f841c30028b3
LucasBrentano <brentano.l@gmail.com>
2022-11-04 11:34:35 -0300

    removed unused print()

--- DinosaurRun/scenes/world/ground/GroundManager.gd
+++ DinosaurRun/scenes/world/ground/GroundManager.gd
@@ -4,5 +4,4 @@ onready var respawn_position := $Position2D
 
 
 func _on_Ground_screen_exited(ground: Ground) -> void:
-	print(str(ground) + " | respawned")
 	ground.global_position = respawn_position.global_position
