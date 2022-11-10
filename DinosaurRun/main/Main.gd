5b8574683f1202e6f18232d51c3f5e9442d05a9a
Lucas V. Moog Brentano <brentano.l@gmail.com>
2022-11-10 19:51:14 -0300

    obstacle increase wip

---
+++ DinosaurRun/main/Main.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+export(float) var speed_increase_vaue := 0.2
+export(float) var obstacle_spawn_rate_increase := 0.02
+
+onready var map := $Map
+onready var obstacle_manager := $ObstacleManager
+
+
+func _on_SpeedIncreaseTimer_timeout():
+	# obstacle_manager.increase_spawn_rate(obstacle_spawn_rate_increase)
+	obstacle_manager.increase_obstacles_speed(speed_increase_vaue)
+	map.increase_world_speed(speed_increase_vaue)
