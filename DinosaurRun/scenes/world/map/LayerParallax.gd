815ea11e9f6ad378e1eb1b93a85d28c040a0d0ed
Lucas V. Moog Brentano <brentano.l@gmail.com>
2022-11-21 21:06:13 -0300

    added parallaxlayer is_moving flag

--- DinosaurRun/scenes/world/map/LayerParallax.gd
+++ DinosaurRun/scenes/world/map/LayerParallax.gd
@@ -1,6 +1,10 @@
 extends ParallaxLayer
 
 export(float) var speed : float = 50
+export(bool) var is_moving : bool = true
 
 func _physics_process(delta: float) -> void:
+	if not is_moving:
+		return
+	
 	motion_offset.x -= speed * delta
--- DinosaurRun/scenes/world/map/Map.gd
+++ DinosaurRun/scenes/world/map/Map.gd
@@ -4,3 +4,6 @@ onready var parallax := $Parallax
 
 func increase_world_speed(speed_increase: float) -> void:
 	parallax.increase_layers_speed(speed_increase)
+
+ 
+
