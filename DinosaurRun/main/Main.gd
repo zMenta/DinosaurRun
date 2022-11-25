b7cae2025563d9aa5f60120a4a46e5f317a62711
Lucas V. Moog Brentano <brentano.l@gmail.com>
2022-11-25 19:41:48 -0300

    font folder

--- DinosaurRun/main/Main.gd
+++ DinosaurRun/main/Main.gd
@@ -2,10 +2,13 @@ extends Node2D
 
 export(float) var speed_increase_vaue := 0.2
 export(float) var obstacle_spawn_rate_increase := 0.001
+export(int) var points_per_timeout := 1
 
 onready var map := $Map
 onready var obstacle_manager := $ObstacleManager
+onready var point_timer := $PointTimer
 
+var points := 0
 
 func _on_SpeedIncreaseTimer_timeout():
 	obstacle_manager.increase_spawn_rate(obstacle_spawn_rate_increase)
@@ -16,3 +19,8 @@ func _on_SpeedIncreaseTimer_timeout():
 func _on_Player_died():
 	map.stop_world()
 	obstacle_manager.stop_obstacles()
+	point_timer.stop()
+
+
+func _on_PointTimer_timeout():
+	points += points_per_timeout
