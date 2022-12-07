a528b49770d67cddaf59683f9cf893b81a9300f5
Lucas Brentano <brentano.l@gmail.com>
2022-12-07 11:19:42 -0300

    basic startGame functionality

--- DinosaurRun/main/Main.gd
+++ DinosaurRun/main/Main.gd
@@ -1,20 +1,31 @@
 extends Node2D
 
-export(float) var speed_increase_vaue := 0.2
+export(float) var speed_increase_value := 0.2
 export(float) var obstacle_spawn_rate_increase := 0.001
 export(int) var points_per_timeout := 1
 
 onready var map := $Map
 onready var obstacle_manager := $ObstacleManager
 onready var point_timer := $PointTimer
+onready var speed_increase_timer := $SpeedIncreaseTimer
 onready var interface := $Interface
+onready var menu := $Menu
 
 var points := 0
 
+func _start_game() -> void:
+	points = 0
+	point_timer.start()
+	speed_increase_timer.start()
+	interface.visible = true
+	menu.visible = false
+	obstacle_manager.start_obstacles()
+
+
 func _on_SpeedIncreaseTimer_timeout():
 	obstacle_manager.increase_spawn_rate(obstacle_spawn_rate_increase)
-	obstacle_manager.increase_obstacles_speed(speed_increase_vaue)
-	map.increase_world_speed(speed_increase_vaue)
+	obstacle_manager.increase_obstacles_speed(speed_increase_value)
+	map.increase_world_speed(speed_increase_value)
 
 
 func _on_Player_died():
@@ -26,3 +37,7 @@ func _on_Player_died():
 func _on_PointTimer_timeout():
 	points += points_per_timeout
 	interface.update_score(points)
+
+
+func _on_Menu_buttonPlay_pressed() -> void:
+	_start_game()
