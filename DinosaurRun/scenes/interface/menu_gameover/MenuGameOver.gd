e66b9dfa1a82a4737649a593cfd1c540283791d5
LucasBrentano <brentano.l@gmail.com>
2022-12-08 09:33:58 -0300

    removed redundant variable in menu gameover

--- DinosaurRun/scenes/interface/menu_gameover/MenuGameOver.gd
+++ DinosaurRun/scenes/interface/menu_gameover/MenuGameOver.gd
@@ -5,14 +5,13 @@ signal buttonGoBack_pressed
 
 onready var label_score_value := $ColorRect/HBoxContainer/LabelScoreValue
 
-var score_value := 0 setget _set_score_value
-
-func _set_score_value(new_value: int) -> void:
-	score_value = new_value
+func set_label_score(score_value: int) -> void:
 	label_score_value.text = str(score_value)
+
 
 func _on_ButtonRestart_pressed() -> void:
 	emit_signal("buttonRestart_pressed")
+
 
 func _on_ButtonGoBack_pressed() -> void:
 	emit_signal("buttonGoBack_pressed")
