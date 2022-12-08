22b94cef9580fd6089ab58e446a59be8b2fd640f
LucasBrentano <brentano.l@gmail.com>
2022-12-08 11:36:09 -0300

    basic resolution selection

--- DinosaurRun/scenes/interface/menu_options/MenuOptions.gd
+++ DinosaurRun/scenes/interface/menu_options/MenuOptions.gd
@@ -1,14 +1,28 @@
 extends Control
 
-export(Array, String) var available_resolutions
+onready var resolution_options_button := $ColorRect/VBoxContainer/Resolution/OptionButtonResolution
 
-onready var resolution_options := $ColorRect/VBoxContainer/Resolution/OptionButtonResolution
+var resolutions: Dictionary = {
+	"1920x1080": Vector2(1920, 1080),
+	"1366x768": Vector2(1366, 768),
+	"1280x720": Vector2(1280, 720),
+	"1024x600": Vector2(1024, 600),
+	}
 
 
 func _ready():
-	for resolution in available_resolutions:
-		resolution_options.add_item(resolution)
+	for resolution in resolutions:
+		resolution_options_button.add_item(resolution)
 
 
 func _on_CheckBox_toggled(button_pressed:bool) -> void:
 	OS.window_fullscreen = button_pressed
+
+
+func _on_CheckBoxBordless_toggled(button_pressed:bool) -> void:
+	OS.window_borderless = button_pressed
+
+
+func _on_OptionButtonResolution_item_selected(index:int) -> void:
+	var window_size: Vector2 = resolutions[resolution_options_button.get_item_text(index)]
+	OS.set_window_size(window_size)
