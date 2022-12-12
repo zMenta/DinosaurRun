58f10824dabe21716bcdaff02712b8cbaf00d382
LucasBrentano <brentano.l@gmail.com>
2022-12-12 17:32:49 -0300

    setttings save system WIP

--- DinosaurRun/scenes/interface/menu_options/MenuOptions.gd
+++ DinosaurRun/scenes/interface/menu_options/MenuOptions.gd
@@ -5,6 +5,7 @@ signal game_settings_saved(save)
 
 onready var resolution_options_button := $ColorRect/VBoxContainer/Resolution/OptionButtonResolution
 onready var button_fullscreen := $ColorRect/VBoxContainer/FullScreen/CheckBox
+onready var button_borderless := $ColorRect/VBoxContainer/Borderless/CheckBoxBordless
 
 
 onready var save: SaveData setget set_save_data
@@ -19,14 +20,18 @@ var resolutions: Dictionary = {
 
 func set_save_data(new_save: SaveData) -> void:
 	save = new_save
+
 	_on_CheckBox_toggled(save.game_settings.fullscreen)
 	button_fullscreen.pressed = save.game_settings.fullscreen
-	 # _on_CheckBoxBordless_toggled(save.game_settings.borderless)
-	# _on_OptionButtonResolution_item_selected(save.game_settings.screen_resolution_id)
+
+	_on_CheckBoxBordless_toggled(save.game_settings.borderless)
+	button_borderless.pressed = save.game_settings.borderless
+
+	_on_OptionButtonResolution_item_selected(save.game_settings.screen_resolution_id)
+	resolution_options_button.text = resolution_options_button.get_item_text(save.game_settings.screen_resolution_id)
 
 
 func _ready():
-	button_fullscreen.toggle_mode = true
 	for resolution in resolutions:
 		resolution_options_button.add_item(resolution)
 
@@ -43,10 +48,11 @@ func _on_OptionButtonResolution_item_selected(index:int) -> void:
 	var window_size: Vector2 = resolutions[resolution_options_button.get_item_text(index)]
 	OS.set_window_size(window_size)
 
-
 func _on_ButtonMainMenu_pressed() -> void:
 	emit_signal("buttonMainMenu_pressed")
 
+	save.game_settings.screen_resolution_id = resolution_options_button.get_selected_id()
 	save.game_settings.fullscreen = button_fullscreen.pressed
+	save.game_settings.borderless = button_borderless.pressed
 
 	emit_signal("game_settings_saved", save)
