785a3834063621d5faeac99e734419703e37ede2
Lucas Brentano <brentano.l@gmail.com>
2022-12-20 14:27:43 -0300

    gameover interface coin container

--- DinosaurRun/scenes/interface/menu_gameover/MenuGameOver.gd
+++ DinosaurRun/scenes/interface/menu_gameover/MenuGameOver.gd
@@ -4,9 +4,15 @@ signal buttonRestart_pressed
 signal buttonGoBack_pressed
 
 onready var label_score_value := $ColorRect/HBoxContainer/LabelScoreValue
+onready var label_coin_container := $ColorRect/HBoxContainer2/CoinContainer
+
 
 func set_label_score(score_value: int) -> void:
 	label_score_value.text = str(score_value)
+
+
+func set_label_coin(coin_value: int) -> void:
+	label_coin_container.set_coin_value(coin_value)
 
 
 func _on_ButtonRestart_pressed() -> void:
