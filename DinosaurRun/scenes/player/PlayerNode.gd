227b35bd7661e00b8bb1cb3451a691be8953746f
Lucas Brentano <brentano.l@gmail.com>
2022-12-21 13:42:38 -0300

    hat textures array

--- DinosaurRun/scenes/player/PlayerNode.gd
+++ DinosaurRun/scenes/player/PlayerNode.gd
@@ -2,7 +2,18 @@ extends Node2D
 
 signal player_died
 
+onready var sprite_hat := $Player/Sprite/SpriteHat
+
 export(Resource) var hats_resource
+var current_hat_index := 0
+
+
+func _ready():
+	set_hat(1)
+
+
+func set_hat(hat_index: int) -> void:
+	sprite_hat.texture = hats_resource.hats_textures[hat_index]
 
 
 func _on_Player_died():
