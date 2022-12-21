bfd90400e81c97abad6b9f1ad7f32f9908d93c23
Lucas Brentano <brentano.l@gmail.com>
2022-12-20 22:11:01 -0300

    coin animations

--- DinosaurRun/scenes/world/coin/Coin.gd
+++ DinosaurRun/scenes/world/coin/Coin.gd
@@ -4,6 +4,14 @@ class_name Coin
 export(float) var speed : float = 120
 export(bool) var is_moving : bool = true
 
+func _ready():
+	randomize()
+	if randi() % 2 == 1:
+		$AnimationPlayer.play("idle")
+	else:
+		$AnimationPlayer.play("idle2")
+
+
 
 func _physics_process(delta: float) -> void:
 	if not is_moving:
@@ -15,7 +23,7 @@ func _physics_process(delta: float) -> void:
 func _on_Coin_body_entered(body: Node) -> void:
 	if body is Player:
 		GlobalSignals.emit_signal("coin_obtained")
-		queue_free()
+		$AnimationPlayer.play("pickup")
 
 
 func _on_VisibilityNotifier2D_screen_exited() -> void:
