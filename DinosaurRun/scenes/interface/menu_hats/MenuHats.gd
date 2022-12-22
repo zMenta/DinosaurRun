6c7615cff663edc7869b570793dd7167bc2a66d1
Lucas Brentano <brentano.l@gmail.com>
2022-12-22 14:05:13 -0300

    hat menu | hats selection on menu

--- DinosaurRun/scenes/interface/menu_hats/MenuHats.gd
+++ DinosaurRun/scenes/interface/menu_hats/MenuHats.gd
@@ -3,14 +3,22 @@ extends Control
 signal buyButton_pressed(save)
 signal mainMenuButton_pressed
 
-export(Resource) var hats_array
+export(Resource) var hats_resource
 onready var hat_texture := $ColorRect/VBoxContainer/HBoxContainer/HatTextureRect
+onready var hat_price_label := $ColorRect/VBoxContainer/HBoxContainer2/PriceValue
 
-var save: SaveData
+var save: SaveData setget set_save_data
+var hat_index: int = 0
+
 
 func _ready() -> void:
-	hats_array = hats_array as Hats
-	hat_texture.texture = hats_array.hats[0].texture
+	hats_resource = hats_resource as Hats
+
+
+func set_save_data(new_save: SaveData) -> void:
+	save = new_save
+	hat_index = save.player_stats.current_hat_index
+	_set_hat_texture(hat_index)
 
 
 func _on_ButtonGoBack_pressed():
@@ -23,7 +31,22 @@ func _on_ButtonBuy_pressed():
 
 
 func _on_ButtonNext_pressed():
-	pass # Replace with function body.
+	if hat_index >= hats_resource.hats.size() - 1:
+		return
+
+	hat_index += 1
+	_set_hat_texture(hat_index)
+
 
 func _on_ButtonPrevious_pressed():
-	pass # Replace with function body.
+	if hat_index <= 0:
+		return
+
+	hat_index -= 1
+	_set_hat_texture(hat_index)
+
+
+func _set_hat_texture(index: int) -> void:
+	var hat = hats_resource.hats[index] as HatItem
+	hat_texture.texture = hat.texture
+	hat_price_label.text = str(hat.price)
