4d16527fd30e3ca75b5a2f31d561c9adeb36417e
Lucas Brentano <brentano.l@gmail.com>
2022-12-23 08:13:22 -0300

    better default saveData screen resolution

--- DinosaurRun/common/resources/GameSettings.gd
+++ DinosaurRun/common/resources/GameSettings.gd
@@ -1,6 +1,6 @@
 class_name GameSettings
 extends Resource
 
-export var screen_resolution_id: int = 0
+export var screen_resolution_id: int = 2
 export var fullscreen: bool = false
 export var borderless: bool = false
